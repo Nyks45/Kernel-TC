583569da04f8e36300958fa1f73618ae0f2383ed
Dev-Yuj <tab.andsupp@gmail.com>
2016-07-18 23:19:44 +0400

    Addition of CLoog,PPl & GDB support

---
+++ lib/gcc/x86_64-linux-gnu/6.1.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --build=x86_64-linux-gnu --target=x86_64-linux-gnu --with-gnu-version=Linarov61 --enable-multiarch --with-cloog --with-isl --with-ppl --with-newlib --enable-languages=c,c++ --enable-graphite=yes --enable-gold=default --disable-option-checking --disable-bootstrap --enable-threads --disable-nls --with-gnu-as --with-gnu-ld --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-eh-frame-hdr-for-static --with-python --enable-cloog-backend=isl --with-libs= --with-tune=intel --with-arch=x86-64 --disable-libssp --enable-multilib-list=m64,m32,mx32 --with-fpmath=sse --enable-lto --enable-shared --enable-plugins --prefix=/mnt/d/install";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "intel" } };
