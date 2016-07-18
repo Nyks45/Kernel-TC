9e78b6f2d2065d57b6b3562eb33c381a6dfdcd03
Dev-Yuj <tab.andsupp@gmail.com>
2016-07-18 23:17:35 +0400

    Cleanse before CLoog & ISL support addition

--- lib/gcc/x86_64-linux-gnu/6.1.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-linaro-snapshot-6.1-2016.07/configure --build=x86_64-linux-gnu --target=x86_64-linux-gnu --with-cloog --with-isl --with-newlib --disable-headers --enable-languages=c,c++ --enable-graphite=yes --enable-python --enable-threads --enable-gold=default --enable-cloog-backend=isl --disable-option-checking --disable-bootstrap --disable-nls --with-gnu-as --with-gnu-ld --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-eh-frame-hdr-for-static --enable-initfini-array --enable-lto --enable-multilib-list=m32,m64,mx32 --with-fpmath=sse --enable-plugins --enable-shared --disable-libssp --with-arch=x86-64 --with-tune=intel --prefix=/mnt/d/install";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "intel" } };
