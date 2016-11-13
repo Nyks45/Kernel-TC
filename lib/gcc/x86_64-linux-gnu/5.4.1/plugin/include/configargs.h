5e3d23f9344c6e2f447bd5edc02f669c585f0d4c
Dev-Yuj <tab.andsupp@gmail.com>
2016-11-13 17:17:28 +0530

    UBERTC with Custom GSR Thanks to @sayeed99

---
+++ lib/gcc/x86_64-linux-gnu/5.4.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --build=x86_64-linux-gnu --target=x86_64-linux-gnu --with-gcc-version=UBER5.3.011/12 --with-cloog --with-isl --with-newlib --with-ppl --enable-languages=c,c++ --enable-graphite=yes --enable-plugins --enable-gold=default --disable-option-checking --disable-bootstrap --enable-threads --with-fpmath=sse --disable-nls --with-gnu-as --with-gnu-ld --enable-lto --enable-multiarch --with-diagnostics-color=always --disable-libstdc__-v3 --enable-eh-frame-hdr-for-static --enable-initfini-array --program-transform-name='s&^&x86_64-linux-&' --with-arch=x86-64 --with-tune=intel --enable-cloog-backend=isl --enable-multilib-list=m32,mx32,m64 --with-sysroot=/ --with-isysroot=/mnt/d/sys --with-python --prefix=/mnt/d/install --disable-libssp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "intel" } };
